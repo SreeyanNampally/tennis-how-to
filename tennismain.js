a466c12ad5ba75b6dda4542ea5031d74cbd3a570
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-04-14 17:47:11 -0400

    Add files via upload

---
+++ tennismain.js
@@ -0,0 +1,18 @@
+function q1_right(){
+    document.getElementById("question_1").innerHTML="correct!!";
+}
+function q2_right(){
+    document.getElementById("question_2").innerHTML="correct!!";
+}
+function q3_right(){
+    document.getElementById("question_3").innerHTML="correct!!";
+}
+function q1_wrong(){
+    document.getElementById("question_1").innerHTML="wrong!!";
+}
+function q2_wrong(){
+    document.getElementById("question_2").innerHTML="wrong!!";
+}
+function q3_wrong(){
+    document.getElementById("question_3").innerHTML="wrong!!";
+}
